8a46493fd7c799d27458f9f92bd05fdc82f75548
wangjianjie <wangjianjie606@gmail.com>
2017-01-26 15:05:07 +0800

    = =

--- assets/js/personal.js
+++ assets/js/personal.js
@@ -1,9 +1,16 @@
 function blogInit(){
 	$("#backBtn").on("click",function(){
-	  history.go(-1);
+		history.go(-1);
 	});
 	
-	$('#menuBtn').on("click",function(a,b,c,d){
-		var asd = 1;
+	$('#menuBtn').on("click",function(){
+		const width = $(this).width();
+		if(width>20){
+			$("#menuBtn").attr("class","closeMenuBtn");
+			$("#leftDiv").attr("class","closeMenu");
+		}else{
+			$("#menuBtn").attr("class","openMenuBtn");
+			$("#leftDiv").attr("class","openMenu");
+		}
 	});
 }
