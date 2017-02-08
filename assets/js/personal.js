f68580f8a15c54157c5331d729acc9a3452541f6
wangjianjie <wangjianjie606@gmail.com>
2017-02-08 14:55:05 +0800

    add google Analysic tools

--- assets/js/personal.js
+++ assets/js/personal.js
@@ -73,4 +73,16 @@ function blogInit(){
 	$("#homeTopBtn").click(()=>{
 		$("html, body, .content").animate({scrollTop:0}); 
 	});
+	
+	
+	
+	
+	/* google analytics*/
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-91619330-1', 'auto');
+	ga('send', 'pageview');
 }
